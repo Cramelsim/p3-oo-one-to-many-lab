b0731d246fffd582e0294195d8a2fd3d24f33c56
MELODY SIMWA <simwamelody@gmail.com>
2025-02-24 13:04:21 +0300

    update owner_pet.py

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,40 @@
 class Pet:
-    pass
+    
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"] 
+    all=[]
+    
+    def __init__(self, name, pet_type ,owner=None):
+        if not isinstance(name, str):
+            raise Exception("Pet name must be a string")
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f"Invalid pet type: {pet_type}")
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("Owner must be an instance of Owner or None")
+        
+        self.name=name
+        self.pet_type=pet_type
+        self.owner=owner
+
+        Pet.all.append(self) 
+#add pet on list
+        if owner:
+           owner.add_pet(self)
 
 class Owner:
-    pass
+    def __init__(self,name):
+       if not isinstance(name, str):
+            raise Exception("Name must be a string")
+       self.name = name
+       self._pets = []  
+       
+    def pets(self):
+        return self._pets
+    
+    def add_pet(self, pet):
+        if not isinstance (pet,Pet):
+            raise Exception ("Pet must be an instance of pet class")
+        pet.owner=self
+        self._pets.append(pet)
+
+    def get_sorted_pets(self):
+         return sorted(self._pets, key=lambda pet: pet.name)
